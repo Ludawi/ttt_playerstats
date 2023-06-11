7cff676d5f5add43990949872415c75a8b8f944d
Ludawi <wistuba9@gmail.com>
2023-06-11 22:47:33 +0200

    reworked all

--- playerstats.lua
+++ playerstats.lua
@@ -1,73 +1,16 @@
-require("net")
-require("player")
-require("hook")
+local currentPlayers = player.GetAll() -- change to GetHumans later
 
--- Initialize the role count table
+local getSteamId = Entity(1):SteamID64()
 
-hook.Add("TTTBeginRound", "PrintMessageAtRoundStart", function()
-  PrintMessage(HUD_PRINTTALK, "A new round has started!")
-end)
+local plyInfo = player.GetBySteamID64(getSteamId)
 
-local roleCount = {}
+local plyCount = player.GetCount()
 
--- Function to send private chat message to player
-local function sendPrivateChatMessage(ply, message)
-  net.Start("TTTPrivateMessage")
-  net.WriteString(message)
-  net.Send(ply)
+for i = 1, plyCount do
+  print(currentPlayers[i], currentPlayers[i]:GetRole())
 end
 
--- Print the round count and role count to the player's chat at the start of each round
-hook.Add("TTTBeginRound", "PrintRoundAndRoleCount", function()
-  for _, ply in ipairs(player.GetAll()) do
-    local steamID = ply:SteamID()
-    local roundCount = tonumber(ply:GetPData("round_count", 0))
-    local innocentCount = tonumber(ply:GetPData("innocent_count", 0))
-    local detectiveCount = tonumber(ply:GetPData("detective_count", 0))
-    local traitorCount = tonumber(ply:GetPData("traitor_count", 0))
-    local message = "You have played " ..
-        roundCount ..
-        " rounds on this server. Role count: Innocent - " ..
-        innocentCount .. ", Detective - " .. detectiveCount .. ", Traitor - " .. traitorCount .. "."
-    ply:PrintMessage(HUD_PRINTTALK, message)
-    ply:PrintMessage(HUD_PRINTCONSOLE, message)
-    sendPrivateChatMessage(ply, message)
-  end
-end)
 
--- Increment the role count when a player's role changes
-hook.Add("TTTUpdatePlayer", "IncrementRoleCount", function(ply)
-  local role = ply:GetRole()
-  local roleString = ROLE_STRINGS[role]
-  if role ~= ROLE_INNOCENT then
-    roleCount[ply:SteamID()] = roleCount[ply:SteamID()] or {}
-    roleCount[ply:SteamID()][roleString] = (roleCount[ply:SteamID()][roleString] or 0) + 1
-    ply:SetPData(roleString:lower() .. "_count", roleCount[ply:SteamID()][roleString])
-  end
-end)
-
--- Increment the round count for each player
-hook.Add("TTTEndRound", "IncrementRoundCount", function(result)
-  for _, ply in ipairs(player.GetAll()) do
-    local roundCount = tonumber(ply:GetPData("round_count", 0))
-    ply:SetPData("round_count", roundCount + 1)
-  end
-end)
-
--- Send private chat message to player with their round count and role count at the start of each round
-hook.Add("TTTBeginRound", "SendPrivateMessage", function()
-  for _, ply in ipairs(player.GetAll()) do
-    local steamID = ply:SteamID()
-    local roundCount = tonumber(ply:GetPData("round_count", 0))
-    local innocentCount = tonumber(ply:GetPData("innocent_count", 0))
-    local detectiveCount = tonumber(ply:GetPData("detective_count", 0))
-    local traitorCount = tonumber(ply:GetPData("traitor_count", 0))
-    local message = "You have played " ..
-        roundCount ..
-        " rounds on this server. Role count: Innocent - " ..
-        innocentCount .. ", Detective - " .. detectiveCount .. ", Traitor - " .. traitorCount .. "."
-    sendPrivateChatMessage(ply, message)
-  end
-end)
-
--- Create the net message to send
+local function getAllRoles(currentPlayers)
+  print("Error: getAllRoles is currently not supported")
+end
